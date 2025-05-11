e7736649abb93e8a11ef4525200977f23ba46cf5
Maggie Kuczynska <maggie.kuczynska@gmail.com>
2025-05-11 13:58:33 +0100

    Adding about app to settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
+    'about',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
