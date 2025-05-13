6645a8e82675f99825c6619d13c7bbced44093fd
Maggie Kuczynska <maggie.kuczynska@gmail.com>
2025-05-13 17:46:50 +0100

    Adding a model of a post in posts app, models.py

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.Charfield(max_length=200)
+    content = models.DateTimeField(auto_now_add=True)
+
+    def_str_(self):
+    return self.title
