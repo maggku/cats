fe244ccc21f457ce94c91bf1442722add262dc6e
Maggie Kuczynska <maggie.kuczynska@gmail.com>
2025-05-14 19:39:15 +0100

    Adding author to the Post model in models.py/posts app

--- posts/models.py
+++ posts/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200,unique=True)
     slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
