54ac04e64d6521d4a6f216d3451bb90332c9d664
Maggie Kuczynska <maggie.kuczynska@gmail.com>
2025-05-14 13:51:16 +0100

    django_heroku.settings(locals())- deleting it from settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-django_heroku.settings(locals())
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
