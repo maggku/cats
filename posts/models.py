685d4b50ad3f969280c50fbf3230cba068a84b4f
Maggie Kuczynska <maggie.kuczynska@gmail.com>
2025-05-14 19:41:40 +0100

    Adding content to the Post model in models.py/posts app

--- posts/models.py
+++ posts/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200,unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
