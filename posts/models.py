d73ce6af869ec61aae6323ca525efc4658cc5931
Maggie Kuczynska <maggie.kuczynska@gmail.com>
2025-05-14 19:36:00 +0100

    adding a Slug component to the Post model in models.py/ posts app, deleting a content component

--- posts/models.py
+++ posts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=200,unique=True)
-    content = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
