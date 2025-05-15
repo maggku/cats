4e32ab2e17d4fd92237004e18a7b9c958170b6ee
Maggie Kuczynska <maggie.kuczynska@gmail.com>
2025-05-15 12:16:00 +0100

    Adding blog.urls path to the urlpatterns in urls.py blog app

--- blog/urls.py
+++ blog/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    
+    path("", include("posts.urls"), name="posts-urls"),
+
+    path('admin/', admin.site.urls),   
 ]
