02d8d7241b621d7c20c5f7225a223f5dd993d960
Maggie Kuczynska <maggie.kuczynska@gmail.com>
2025-05-15 12:30:44 +0100

    Deleting content from views.py posts app

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
-from .models import Post
-
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'posts/post_list.html', {'posts': posts})
