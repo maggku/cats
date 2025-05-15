19a0d3809051150831d59e5a059659d771c78837
Maggie Kuczynska <maggie.kuczynska@gmail.com>
2025-05-15 09:04:24 +0100

    Adding a Comment model to models.py posts app

--- posts/models.py
+++ posts/models.py
@@ -15,5 +15,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comments")  
+    body = models.TextField()
+    approved = models.BooleanField(default=False) 
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.post.title}"
+
         
     
