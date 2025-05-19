f881646ff7cb70635175723b647639da3301e08b
Maggie Kuczynska <maggie.kuczynska@gmail.com>
2025-05-19 14:35:40 +0100

    Adding a SITE_ID of 1 in settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -51,6 +51,11 @@ INSTALLED_APPS = [
     'django_summernote',
     'posts',
     'about',
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
