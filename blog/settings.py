5d19ed5edafe2564ca55ffe2310970166ad929a7
Maggie Kuczynska <maggie.kuczynska@gmail.com>
2025-05-19 14:47:54 +0100

    Including the path of accounts in the urlpatterns

--- blog/settings.py
+++ blog/settings.py
@@ -57,7 +57,7 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
