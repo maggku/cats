e967cd46e5055532f3187716cf4f988e2dfc12c6
Maggie Kuczynska <maggie.kuczynska@gmail.com>
2025-05-20 12:07:35 +0100

    In posts/views.py, within the post_detail view, creating a variable comment_form and assign the class CommentForm() to it

--- posts/views.py
+++ posts/views.py
@@ -28,6 +28,7 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_at")
     comment_count = post.comments.filter(approved=True).count()
+    comment_form = CommentForm()
 
     return render(
         request,
@@ -36,5 +37,6 @@ def post_detail(request, slug):
             "post": post,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
         },
     )
