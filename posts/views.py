0a6a05dc65fe87df726040e115d12280d9263ef5
Maggie Kuczynska <maggie.kuczynska@gmail.com>
2025-05-20 11:54:26 +0100

    Installing the two packages django-crispy-forms and crispy-bootstrap.

--- posts/views.py
+++ posts/views.py
@@ -25,12 +25,12 @@ def post_detail(request, slug):
 
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-    comments = post.comments.all().order_by("-created_on")
+    comments = post.comments.all().order_by("-created_at")
     comment_count = post.comments.filter(approved=True).count()
 
     return render(
         request,
-        "blog/post_detail.html",
+        "posts/post_detail.html",
         {
             "post": post,
             "comments": comments,
