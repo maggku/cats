4ca052849ee05707f02be4de4e1afe47d39442f8
Maggie Kuczynska <maggie.kuczynska@gmail.com>
2025-06-05 17:50:28 +0100

    Adding secret_key to the Heroku account

--- blog/settings.py
+++ blog/settings.py
@@ -28,7 +28,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6j2@#o$)8^t0^*k5cb-+4dak4@px5b21xl&^fs7r!zyk=$0j^k'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=True
