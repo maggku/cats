16235df4e53514fedb3e7b1164bd29323e45a183
Maggie Kuczynska <maggie.kuczynska@gmail.com>
2025-06-19 11:07:58 +0100

    Changing to DEBUG=False in settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -31,7 +31,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG=True
+DEBUG=False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
 
